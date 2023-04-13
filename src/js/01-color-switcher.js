ceb46d75369beb96c3761af7a9d00bda3e491fd5
ViTorri <45861551+ViTorri@users.noreply.github.com>
2023-04-13 23:24:16 +0100

    first-task-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector("button[data-start]");
+console.log(startBtn);
+const stoptBtn = document.querySelector("button[data-stop]");
+console.log(stoptBtn);
+const body = document.body;
+console.log(body);
 
+let timerId = null;
+
+const onClickStartBtn = () => {
+    startBtn.disabled = true;
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        body.style.backgroundColor = color;
+    }, 1000);
+};
+
+const onClickStopBtn = () => {
+    startBtn.disabled = false;
+    clearInterval(timerId);
+}
+
+startBtn.addEventListener("click", onClickStartBtn);
+stoptBtn.addEventListener("click", onClickStopBtn);
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
