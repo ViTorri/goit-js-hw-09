b57865be008f8691ad764b4ff648104cf2716cfc
ViTorri <45861551+ViTorri@users.noreply.github.com>
2023-05-30 15:54:28 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,29 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+    delayInput: document.querySelector('input[name="delay"]'),
+    stepInput: document.querySelector('input[name="step"]'),
+    amountInput: document.querySelector('input[name="amount"]'),
+    createBtn: document.querySelector('button[type = "submit"]'),
 }
+console.log(refs);
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   setTimeout(() => {
+// if (shouldResolve) {
+//   resolve('✅');
+//   } else {
+//   reject('❌');
+//   }
+//   })
+// }
+
+// createPromise(position, delay)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
